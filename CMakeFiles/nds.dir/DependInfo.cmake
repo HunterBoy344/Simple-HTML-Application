fdbbb51cde86214cbf991bb037f1072635a6b9f8
HunterBoy344 <66140471+HunterBoy344@users.noreply.github.com>
2023-05-27 15:24:39 -0400

    Summary (required)
    Description

---
+++ CMakeFiles/nds.dir/DependInfo.cmake
@@ -0,0 +1,103 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/libretro-common/encodings/encoding_utf.c" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/libretro-common/encodings/encoding_utf.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_LIBZ=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/libretro-common/include"
+  "../src/utils"
+  "../src/metaspu"
+  "."
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/main.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/main.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/Database.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/Database.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/FIFO.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/FIFO.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/GPU.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/GPU.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/MMU.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/MMU.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/NDSSystem.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/NDSSystem.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/ROMReader.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/ROMReader.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/SPU.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/SPU.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1_none.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1_none.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1_retail_auto.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1_retail_auto.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1_retail_mcrom.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1_retail_mcrom.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1_retail_nand.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1_retail_nand.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1comp_mc.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1comp_mc.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1comp_protocol.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1comp_protocol.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1comp_rom.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot1comp_rom.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot2_none.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/addons/slot2_none.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/arm_instructions.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/arm_instructions.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/arm_jit.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/arm_jit.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/armcpu.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/armcpu.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/bios.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/bios.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/cheatSystem.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/cheatSystem.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/commandline.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/commandline.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/common.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/common.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/cp15.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/cp15.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/debug.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/debug.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/driver.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/driver.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/emufile.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/emufile.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/encrypt.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/encrypt.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/filter/deposterize.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/filter/deposterize.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/filter/xbrz.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/filter/xbrz.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/firmware.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/firmware.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/gfx3d.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/gfx3d.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/matrix.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/matrix.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/mc.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/mc.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/metaspu/metaspu.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/metaspu/metaspu.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/mic.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/mic.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/movie.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/movie.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/path.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/path.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/rasterize.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/rasterize.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/readwrite.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/readwrite.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/render3D.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/render3D.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/rtc.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/rtc.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/saves.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/saves.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/slot1.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/slot1.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/slot2.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/slot2.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/texcache.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/texcache.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/thumb_instructions.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/thumb_instructions.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/utils/colorspacehandler/colorspacehandler.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/utils/colorspacehandler/colorspacehandler.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/utils/datetime.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/utils/datetime.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/utils/decrypt/decrypt.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/utils/decrypt/decrypt.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/utils/decrypt/header.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/utils/decrypt/header.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/utils/guid.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/utils/guid.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/utils/xstring.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/utils/xstring.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/version.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/version.o"
+  "/root/StuntsMultiplayer/desmume-wasm/desmume/src/wifi.cpp" "/root/StuntsMultiplayer/desmume-wasm/desmume/wasm-port/CMakeFiles/nds.dir/root/StuntsMultiplayer/desmume-wasm/desmume/src/wifi.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_LIBZ=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/libretro-common/include"
+  "../src/utils"
+  "../src/metaspu"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
